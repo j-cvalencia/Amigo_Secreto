e731897badb95caaec7ec714e982f2f4ec93e4a1
Juan Valencia <valenciacamilo5926@gmail.com>
2025-01-27 11:36:17 -0500

    Desafio amigo secreto

---
+++ app.js
@@ -0,0 +1,33 @@
+let amigos = [];
+
+function agregarAmigo(){
+  let inputValue = document.querySelector('#amigo').value;
+  if (inputValue.trim() !== '') {
+    amigos.push(inputValue.trim());
+    document.querySelector('#amigo').value = '';
+    recorrerArray();
+  } else {
+    alert('Porfavor insertar un nombre');
+  }
+}
+
+function recorrerArray(){
+  let listaAmigos = document.querySelector('#listaAmigos');
+  listaAmigos.innerHTML = '';
+  amigos.forEach((amigo)=>{
+    let li = document.createElement('li');
+    li.textContent = amigo;
+    listaAmigos.appendChild(li);
+  })
+}
+
+function sortearAmigo(){
+  if (amigos.length > 0){
+    let numero = Math.floor(Math.random()*amigos.length);
+    let li = document.createElement('li');
+    li.textContent = amigos[numero];
+    document.querySelector('#resultado').appendChild(li);
+  } else {
+    alert('No hay amigos en el sorteo')
+  }
+}
