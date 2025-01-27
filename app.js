1e61164f448d9e98f2ab5581ab46c383f6979155
Juan Valencia <valenciacamilo5926@gmail.com>
2025-01-27 11:51:17 -0500

    Se arregló un pequeño error que tenia al dar el resultado

--- app.js
+++ app.js
@@ -26,7 +26,12 @@ function sortearAmigo(){
     let numero = Math.floor(Math.random()*amigos.length);
     let li = document.createElement('li');
     li.textContent = amigos[numero];
-    document.querySelector('#resultado').appendChild(li);
+    if (document.querySelector('#resultado').children[0]) {
+      document.querySelector('#resultado').children[0].remove();
+      document.querySelector('#resultado').appendChild(li);
+    } else {
+      document.querySelector('#resultado').appendChild(li);
+    }
   } else {
     alert('No hay amigos en el sorteo')
   }
